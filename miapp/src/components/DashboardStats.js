cd3d666acd506b32d1c5ef39f1102879efc11019
Martin Abramovich <abramovich.martin@gmail.com>
2023-10-15 01:03:36 -0300

    cambio en count

--- miapp/src/components/DashboardStats.js
+++ miapp/src/components/DashboardStats.js
@@ -9,9 +9,9 @@ function DashboardStats() {
     fetch('http://localhost:3001/api/users') // Revisa esto bien
       .then((response) => response.json())
       .then((data) => {
-        if (data && data.users && data.users.length > 0) {
+        if (data && data.count) {
           // Selecciona el último elemento del array de usuarios
-          const ultimo = data.users[data.users.length - 1];
+          const ultimo = data.count;
           setUltimoUsuario(ultimo);
         }
       })
@@ -21,12 +21,11 @@ function DashboardStats() {
   }, []);
 
   useEffect(() => {
-    fetch('http://localhost:3001/api/products') // verifica que es correcta
+    fetch('http://localhost:3001/api/products')
       .then((response) => response.json())
       .then((data) => {
-        if (data && data.products && data.products.length > 0) {
-          // Selecciona el último elemento del array de productos
-          const ultimoP = data.products[data.products.length - 1];
+        if (data && data.count) { // Verifica que 'count' existe en el objeto data
+          const ultimoP = data.count;
           setUltimoProducto(ultimoP);
         }
       })
@@ -47,7 +46,7 @@ function DashboardStats() {
                 <div className="text-xs font-weight-bold text-primary text-uppercase mb-1">
                   Products in Data Base
                 </div>
-                <div className="h5 mb-0 font-weight-bold text-gray-800">{ultimoProducto ? ultimoProducto.id : "Cargando..."}</div>
+                <div className="h5 mb-0 font-weight-bold text-gray-800">{ultimoProducto ? ultimoProducto : "Cargando..."}</div>
               </div>
               <div className="col-auto">
                 <i className="fas fa-clipboard-list fa-2x text-gray-300"></i>
@@ -80,7 +79,7 @@ function DashboardStats() {
               <div className="col mr-2">
                 <div className="text-xs font-weight-bold text-warning text-uppercase mb-1">Users quantity</div>
                 <div className="h5 mb-0 font-weight-bold text-gray-800">
-                  {ultimoUsuario ? ultimoUsuario.id : "Cargando..."}
+                  {ultimoUsuario ? ultimoUsuario : "Cargando..."}
                 </div>
               </div>
               <div className="col-auto">
