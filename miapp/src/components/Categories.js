c57e900ecb751a1d76a58372ebb9ed4a6b7f9f45
Daniel26jk <danielmoralese26@gmail.com>
2023-10-15 12:14:31 -0300

    Categories
    arreglado

--- miapp/src/components/Categories.js
+++ miapp/src/components/Categories.js
@@ -2,36 +2,46 @@ import React, { useState, useEffect } from 'react';
 import "../assets/style.css"
 
 function Categories() {
-  const [categoryName, setCategoryName] = useState('');
+  const [categoryNames, setCategoryNames] = useState([]);
 
   useEffect(() => {
-    
-    fetch('http://localhost:3001/api/categories') 
+    fetch('http://localhost:3001/api/categories')
       .then((response) => response.json())
       .then((data) => {
         if (data && data.data && data.data.length > 0) {
-          const firstCategory = data.data[0];
-          setCategoryName(firstCategory.nombre);
+          // Trae todos los nombres de categoría en un arreglo
+          const names = data.data.map(category => category.nombre); //no es for es map c:<
+          setCategoryNames(names);
         }
       })
       .catch((error) => {
-        console.error('Error al cargar la categoría:', error);
+        console.error('Error al cargar las categorías:', error);
       });
   }, []);
 
   return (
-    <div className="col-lg-6 mb-4" style={{ maxWidth: "none", flex: "none" }} >
+    <div className="col-lg-6 mb-4" style={{ maxWidth: "none", flex: "none" }}>
       <div className="card shadow mb-4">
         <div className="card-header py-3">
           <h6 className="m-0 font-weight-bold text-primary">Categories in Data Base</h6>
         </div>
         <div className="card-body">
-          <div className="row">
-            <div className="col-lg-6 mb-4">
-              <div className="card bg-info text-white shadow">
-                <div className="card-body">{categoryName || 'Cargando...'}</div>
+          <div className="row"> {/* se necesita la key=index sino salta un error  */}
+            {categoryNames.length > 0 ? (
+              categoryNames.map((name, index) => (
+                <div key={index} className="col-lg-6 mb-4">
+                  <div className="card bg-info text-white shadow">
+                    <div className="card-body">{name}</div>
+                  </div>
+                </div>
+              ))
+            ) : (
+              <div className="col-lg-6 mb-4">
+                <div className="card bg-info text-white shadow">
+                  <div className="card-body">Cargando...</div>
+                </div>
               </div>
-            </div>
+            )}
           </div>
         </div>
       </div>
