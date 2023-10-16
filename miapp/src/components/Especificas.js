65e1a6a2df81f312349005a0b485e65fbdea31ce
Daniel26jk <danielmoralese26@gmail.com>
2023-10-16 09:44:40 -0300

    a
    a

---
+++ miapp/src/components/Especificas.js
@@ -0,0 +1,49 @@
+import React, { useState, useEffect } from 'react';
+
+function LastUsers() {
+  const [LastUsers, setLastUsers] = useState(null);
+
+  useEffect(() => {
+   
+    fetch('http://localhost:3001/api/users') 
+      .then((response) => response.json())
+      .then((data) => {
+        if (data && data.users && data.users.length > 0) {
+          const ultimoProducto = data.users[data.findUser.length - 1];
+          setLastUsers(ultimoProducto);
+        }
+      })
+      .catch((error) => {
+        console.error('Error al cargar productos:', error);
+      });
+      // luego solo llama lo que quieres buscar de la api del back .imagen .nombre .descripcion
+  }, []);
+
+  return (
+    <div className="col-lg-6 mb-4" style={{ maxWidth: "none", flex:"none" }}>
+      <div className="card shadow mb-4">
+        <div className="card-header py-3">
+          <h6 className="m-0 font-weight-bold text-primary">Último producto agregado</h6>
+        </div>
+        <div className="card-body">
+          <div className="text-center">
+            <img
+              className="img-fluid px-3 px-sm-4 mt-3 mb-4"
+              style={{ width: '25rem' }}
+              src={LastUsers ? LastUsers.imagen : 'assets/images/product_dummy.svg'}
+              alt={LastUsers ? LastUsers.nombre : 'image dummy'}
+            />
+          </div>
+          <p>
+            {LastUsers ? LastUsers.email : 'Cargando...'}
+          </p>
+          <a href="/" target="_blank" rel="noopener noreferrer">
+            Ver detalle de producto
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default LastUsers;
